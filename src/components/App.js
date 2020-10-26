f95f08f6c451719d3921cc7ced64f85f575bee5e
Pratham Singh <prathamsingh96@gmail.com>
2020-10-26 13:15:51 +0530

    major changes

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import "../styles/App.css";
+
+import React, { Component } from "react";
+import Board from "./Board";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">React Trello App</div>
+
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default App;
